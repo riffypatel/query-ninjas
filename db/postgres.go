5a0721026d6c5608a091e078fe0d0c298529c8ea
Mina <mnaq006@gmail.com>
2026-03-15 19:54:43 +0000

    Fixed CreateBusinessProfile service and tested successfully

--- db/postgres.go
+++ db/postgres.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -13,8 +14,13 @@ import (
 var DB *gorm.DB
 
 func InitDb() {
+	// load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	//connect to database
-	var err error
 	connStr := fmt.Sprintf(
 		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
 		os.Getenv("DB_HOST"),
--- services/business_profile_service.go
+++ services/business_profile_service.go
@@ -10,15 +10,8 @@ type BusinessService struct {
 	Repo repository.BusinessProfileRepository
 }
 
-//function to create a new business profile
-
+// function to create a new business profile
 func (s *BusinessService) CreateBusinessProfile(req *models.BusinessProfile) error {
-
-	// Check if business already exists
-	_, err := s.Repo.GetBusinessProfile(req.ID)
-	if err == nil {
-		return errors.New("business already exists")
-	}
 
 	//Check if business name or email is empty
 	if req.BusinessName == "" {
@@ -29,16 +22,14 @@ func (s *BusinessService) CreateBusinessProfile(req *models.BusinessProfile) err
 		return errors.New("business email is required")
 	}
 
-	//Save business profile to database
-	err = s.Repo.CreateBusinessProfile(req)
+	err := s.Repo.CreateBusinessProfile(req)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-//function to get business profile
-
+// function to get business profile
 func (s *BusinessService) GetBusinessProfile(id uint) (*models.BusinessProfile, error) {
 	profile, err := s.Repo.GetBusinessProfile(id)
 	if err != nil {
@@ -47,15 +38,14 @@ func (s *BusinessService) GetBusinessProfile(id uint) (*models.BusinessProfile, 
 	return profile, nil
 }
 
-//function to update business profile
-
+// function to update business profile
 func (s *BusinessService) UpdateBusinessProfile(req *models.BusinessProfile) error {
 
-    // Check if business profile exists
-    _, err := s.Repo.GetBusinessProfile(req.ID)
-    if err != nil {
-        return errors.New("business profile not found")
-    }
+	// Check if business profile exists
+	_, err := s.Repo.GetBusinessProfile(req.ID)
+	if err != nil {
+		return errors.New("business profile not found")
+	}
 
 	//Check if business name or email is empty
 	if req.BusinessName == "" {
